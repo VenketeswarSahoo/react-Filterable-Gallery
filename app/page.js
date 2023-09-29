23deb84985c87cc458b9f642e01894d6e93f2372
Venkatesh <sahoovenketeswar@gmail.com>
2023-09-30 00:15:08 +0530

    This is a Filterable Gallery made using react

--- app/page.js
+++ app/page.js
@@ -1,95 +1,76 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import React, { useState } from "react";
 
-export default function Home() {
+function ImageFilter() {
+  const [activeFilter, setActiveFilter] = useState("all");
+
+  const filterableData = [
+    {
+      name: "nature",
+      src:
+        "https://img.freepik.com/free-vector/landscape-man-canoe-river_24877-76260.jpg?w=740&t=st=1694446052~exp=1694446652~hmac=4fc7e9ca0518e8e388fa624cd489f6c10b78a64af4282e4dd655cc634ab2d8b9",
+      title: "Mountains",
+      text: "Lorem ipsum dolor.."
+    },
+    {
+      name: "cars",
+      src:
+        "https://img.freepik.com/free-vector/modern-blue-urban-adventure-suv-vehicle-illustration_1344-205.jpg?w=740&t=st=1694446118~exp=1694446718~hmac=0a6e41b6fae424827675b4812511df7be26f56b27431f4ddf468905143affd05",
+      title: "BMW",
+      text: "Lorem ipsum dolor.."
+    },
+    {
+      name: "people",
+      src:
+        "https://img.freepik.com/free-vector/group-young-people-posing-photo_52683-18823.jpg?w=740&t=st=1694447776~exp=1694448376~hmac=ba75301c83a4c9e8d013881f9915db30d081f8c02cc3cb7b7b79b7ca8cc068f4",
+      title: "Person 1",
+      text: "Lorem ipsum dolor.."
+    }
+    // Add more data for other images/cards
+  ];
+
+  const handleFilterClick = filter => {
+    setActiveFilter(filter);
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="image-filter-container">
+      <div className="filter-buttons" id="filter-buttons">
+        {["all", "cars", "nature", "people"].map(filter =>
+          <button
+            key={filter}
+            className={`btn-filter ${activeFilter === filter
+              ? "active-filter"
+              : ""}`}
+            onClick={() => handleFilterClick(filter)}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+            {filter === "all" ? "Show all" : filter}
+          </button>
+        )}
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="filterable-cards" id="filterable-cards">
+        {filterableData.map((item, index) =>
+          <div
+            key={index}
+            className={`card-filter ${activeFilter === "all" ||
+            activeFilter === item.name
+              ? ""
+              : "hide"}`}
+          >
+            <img src={item.src} alt={item.title} />
+            <div>
+              <h6>
+                {item.title}
+              </h6>
+              <p>
+                {item.text}
+              </p>
+            </div>
+          </div>
+        )}
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
+
+export default ImageFilter;
